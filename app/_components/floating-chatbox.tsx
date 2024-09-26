4dd7b9a3f1125a0f4a99d16372f5bdbf04850d0f
Paul Cedrick Artigo <paulcedrick.artigo@gmail.com>
2024-09-26 22:27:09 +0800

    fix the issue of chatbox height after submitting the message

---
+++ app/_components/floating-chatbox.tsx
@@ -0,0 +1,59 @@
+import Container from "@/components/ui/container";
+import { Textarea } from "@/components/ui/textarea";
+import { useChat } from "ai/react";
+import { memo, useRef } from "react";
+
+function FloatingChatbox({
+  handleSubmit,
+  handleInputChange,
+  input,
+}: {
+  handleSubmit: ReturnType<typeof useChat>["handleSubmit"];
+  handleInputChange: ReturnType<typeof useChat>["handleInputChange"];
+  input: ReturnType<typeof useChat>["input"];
+}) {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  function resetInputHeight() {
+    if (ref.current) {
+      ref.current.style.height = "initial";
+    }
+  }
+
+  function adjustInputHeight() {
+    if (ref.current) {
+      ref.current.style.height = `${ref.current.scrollHeight}px`;
+    }
+  }
+
+  return (
+    <div className="fixed bottom-0 w-full left-0 py-12">
+      <Container>
+        <form onSubmit={handleSubmit}>
+          <Textarea
+            ref={ref}
+            onChange={(ev) => {
+              console.log({ value: ev.target.value });
+              handleInputChange(ev);
+              adjustInputHeight();
+            }}
+            value={input}
+            onKeyDown={(ev) => {
+              if (!ev.shiftKey && ev.code === "Enter") {
+                ev.preventDefault();
+                handleSubmit();
+
+                resetInputHeight();
+              }
+            }}
+            placeholder="Type your message here then press enter if you want to send your message or shift + enter for new line..."
+            className="bg-zinc-800 border-zinc-200 resize-none max-h-60"
+            rows={1}
+          />
+        </form>
+      </Container>
+    </div>
+  );
+}
+
+export default memo(FloatingChatbox);
--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,25 @@
 "use client";
 
 import Container from "@/components/ui/container";
-import { Textarea } from "@/components/ui/textarea";
 import { useChat } from "ai/react";
-import { useRef } from "react";
+import FloatingChatbox from "./_components/floating-chatbox";
 import MessagesPanel from "./_components/messages-panel";
 
 export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
-
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const { messages, input, handleInputChange, handleSubmit, setInput } =
+    useChat();
 
   return (
     <Container className="min-h-screen relative">
       {/* <p>Hello</p> */}
       <MessagesPanel messages={messages} />
 
-      <div className="fixed bottom-0 w-full left-0 py-12">
-        <Container>
-          <form onSubmit={handleSubmit}>
-            <Textarea
-              ref={textAreaRef}
-              onChange={(ev) => {
-                handleInputChange(ev);
-                if (textAreaRef.current) {
-                  textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
-                }
-              }}
-              value={input}
-              onKeyDown={(ev) => {
-                if (!ev.shiftKey && ev.code === "Enter") {
-                  handleSubmit();
-
-                  if (textAreaRef.current) {
-                    textAreaRef.current.style.height = "initial";
-                  }
-                }
-              }}
-              placeholder="Type your message here then press enter if you want to send your message or shift + enter for new line..."
-              className="bg-zinc-800 border-zinc-200 resize-none max-h-60"
-              rows={1}
-            />
-          </form>
-        </Container>
-      </div>
+      <FloatingChatbox
+        input={input}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        setInput={setInput}
+      />
     </Container>
   );
 }
